4dfb6cc924052e585734f0e314747c88a0e15cab
dandrecollera <dandrecollera@gmail.com>
2024-05-08 02:18:25 +0800

    WebP Compiler

---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const sharp = require("sharp");
+
+const inputFolder = "inputs";
+const outputFolder = "output100";
+
+// Create output folder if it doesn't exist
+if (!fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder);
+}
+
+// Get a list of files in the input folder
+fs.readdir(inputFolder, (err, files) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    files.forEach((file) => {
+        if (file.endsWith(".png") || file.endsWith(".jpg")) {
+            const inputPath = `${inputFolder}/${file}`;
+            const outputPath = `${outputFolder}/${file.replace(/\.[^/.]+$/, "")}.webp`;
+
+            sharp(inputPath)
+                .webp({ quality: 100, alphaQuality: 100 })
+                .toFile(outputPath)
+                .then(() => console.log(`Converted ${file} to WebP`))
+                .catch((err) => console.error(err));
+        }
+    });
+});
